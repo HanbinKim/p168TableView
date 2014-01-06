09b6c6119f654007ab20a39715c831630acd2334
HanbinKim <hottuc@naver.com>
2014-01-06 14:39:56 +0900

    p168 TableView 완료하였습니다.
    완료하였습니다..

--- p168TableView/ViewController.m
+++ p168TableView/ViewController.m
@@ -5,19 +5,36 @@
 //  Created by SDT-1 on 2014. 1. 6..
 //  Copyright (c) 2014년 SDT-1. All rights reserved.
 //
-
+#define CELL_ID @"CELL_ID"
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDataSource>
+{
+    NSArray *data;
+}
 @end
 
 @implementation ViewController
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    cell.textLabel.text = data[indexPath.row];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [data count];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    data = @[@"a",@"b",@"c",@"d",@"e",@"f",@"g",@"h",@"i",@"j",@"k",@"l",@"m",@"n",@"o",@"p"];
+    
 }
 
 - (void)didReceiveMemoryWarning
